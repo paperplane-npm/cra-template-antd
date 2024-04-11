e663dd0440b8c1cd5d847f485a8ddb629db56a5c
jia-niang <1@paperplane.cc>
2024-04-11 22:56:32 +0800

    feat(tailwindcss): 提供 tailwindcss 配置

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,15 @@ dayjs.locale('zh-cn')
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
+/**
+ * 如果遇到了无法使用 tailwindcss 覆盖组件库样式的问题，参考：
+ * https://ant-design.antgroup.com/docs/react/compatible-style-cn
+ *
+ * 可以使用组件：
+ * <StyleProvider hashPriority="high"></StyleProvider>
+ * 来包裹入口代码
+ */
+
 root.render(
   <React.Fragment>
     <ConfigProvider locale={zhCN} theme={{}}>
